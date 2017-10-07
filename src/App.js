e404e8fa2be52ed0e1c0ef11bca5490907a87f58
William Meger <wmeger14@gmail.com>
2017-10-07 16:40:34 -0400

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChordExample from './ChordExample';
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
