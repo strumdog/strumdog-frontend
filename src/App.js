b9eaa94840d28f64266be14306e721d756a070b9
William Meger <wmeger14@gmail.com>
2017-10-07 17:18:05 -0400

    revised router imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router,
+    Route,
+    Link,
+    IndexRoute,
+    browserHistory } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import SongEditor from './SongEditor';
