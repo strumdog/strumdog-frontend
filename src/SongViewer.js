390e46aa7cbbcf8586349cc4bf74197a221e7664
Paul Melnikow <email@paulmelnikow.com>
2017-10-08 08:40:36 -0400

    See chord tabs over each line

--- src/SongViewer.js
+++ src/SongViewer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Parser, Chord } from 'react-chord-parser';
 import { fingeringForChord } from './chordMap';
+import groupBy from 'lodash.groupby';
+import './SongViewer.css';
 
 class SongViewer extends Component {
 
@@ -28,7 +30,6 @@ class SongViewer extends Component {
 
         props.client.getSong(id)
             .then(song => {
-                console.log(JSON.stringify(song));
                 const chordsString = song.chords.map(info => info.chord).join(' ');
                 this.setState({
                     title: song.title,
@@ -43,15 +44,55 @@ class SongViewer extends Component {
 
     }
 
+    static groupedLines (lyrics, chords) {
+        const groupedChords = groupBy(chords, info => info.line - 1);
+
+        const result = [];
+
+        for (let i = 0; i < lyrics.length; ++i) {
+            result.push({
+                lyrics: lyrics[i],
+                chords: groupedChords[i] || [],
+            });
+        }
+
+        return result;
+    }
+
+    renderChordContainer (chords) {
+        const characterWidth = 0.6 * 36;
+
+        const chordStyle = left => ({
+            left: characterWidth * (left - 1) - 18 /* about the left margin of the tab */,
+            position: 'absolute',
+            width: 75,
+            height: 85,
+        });
+
+        return (
+            <div className="chordContainer">
+                { chords.map((chord, i) => (
+                    <Chord
+                        key={ i }
+                        style={ chordStyle(chord.position) }
+                        name={ chord.chord }
+                        diagram={ fingeringForChord(chord.chord) }/>
+                )) }
+            </div>
+        );
+    }
+
     render() {
+        const groupedLines = this.constructor.groupedLines(this.state.lyrics, this.state.chords);
+
         return (
             <div>
                 <h1>{this.state.title}</h1>
-                { this.state.uniqueChords.map(chord => (
-                        <Chord
-                            key={ chord }
-                            name={ chord }
-                            diagram={ fingeringForChord(chord) }/>
+                { groupedLines.map((line, i) => (
+                    <div className="lyricLine" key={ i }>
+                        { this.renderChordContainer(line.chords) }
+                        <div className="lyric">{ line.lyrics }</div>
+                    </div>
                 ))}
             </div>
         )
