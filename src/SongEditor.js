0c1eefa68a4db65f2cbec23e7bcb19156d338608
apjansing <apjansing@gmail.com>
2017-10-07 21:52:31 -0400

    added use of flat to sharp map

--- src/SongEditor.js
+++ src/SongEditor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Parser, Chord } from 'react-chord-parser';
 import PropTypes from 'prop-types';
+import { flatToSharp } from './flatToSharpMap';
 import { fingeringForChord } from './chordMap';
 
 class SongEditor extends Component {
@@ -21,7 +22,7 @@ class SongEditor extends Component {
             <Chord
                 key={ chord }
                 name={ chord }
-                diagram={ fingeringForChord(chord) }/>
+                diagram={ flatToSharp( fingeringForChord(chord) ) }/>
         ));
     }
 
