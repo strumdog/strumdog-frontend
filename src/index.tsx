6cb5a6a256d9a829ec6b29d929f57b829e770ed7
Paul Melnikow <github@paulmelnikow.com>
2018-07-24 16:24:11 -0500

    Typescript + prettier (#35)

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
